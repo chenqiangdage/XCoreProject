675b7996b68c420d681f90d7d4d9e892855da97d
chenqiang <112344444@qq.com>
2020-04-02 16:52:37 +0800

    阿里云oss 操作

---
+++ XCoreProject.Api.Common/AliYun/AliYunOss.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Aliyun.OSS;
+using Aliyun.OSS.Common;
+
+namespace XCoreProject.Api.Common.AliYun
+{
+    public class AliYunOss
+    {
+        public static OssClient client;        
+        public AliYunOss()
+        {
+            InitAliYunOss();
+        }
+
+        public void InitAliYunOss()
+        {
+            // 创建ClientConfiguration实例，按照您的需要修改默认参数。
+            var conf = new ClientConfiguration();
+            // 开启支持CNAME。CNAME是指将自定义域名绑定到存储空间上。
+            conf.IsCname = true;             
+            conf.MaxErrorRetry = 3;
+            conf.ConnectionTimeout = 3000;
+            client = new OssClient(AliYunOssConfig.Endpoint,AliYunOssConfig.AccessKeyId,AliYunOssConfig.AccessKeySecret, conf);
+        }
+
+        public bool PutFileToOss(Stream FileStream,string FileKey)
+        {            
+            try
+            {
+                // 上传文件。
+                PutObjectResult pubResult = client.PutObject(AliYunOssConfig.BucketName, FileKey, FileStream);
+                if (pubResult.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Console.WriteLine("Put object succeeded :" + pubResult.ETag);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Put object failed, {0}", ex.Message);
+            }
+            return false;
+        }
+    }
+}
---
+++ XCoreProject.Api.Common/AliYun/AliYunOssConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XCoreProject.Api.Common.AliYun
+{
+    public static class AliYunOssConfig
+    {
+        public static string AccessKeyId = "LTAI4FsHxD8PNeH3Q677EnGU";
+
+        public static string AccessKeySecret = "feys6NNm9CRQQK4ansKSXZ1e2k24vD";
+
+        public static string Endpoint = "https://mnfile.mmdd168.com";
+
+        public static string BucketName = "miaomuduoduo";
+               
+    }
+}
